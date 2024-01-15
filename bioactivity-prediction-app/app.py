908ccca9bc04614e4d97b94016a0cc87c42ecbdd
dev1ashish <93542045+dev1ashish@users.noreply.github.com>
2024-01-15 23:20:55 +0530

    Update app.py
    descriptive changes in frontend

--- bioactivity-prediction-app/app.py
+++ bioactivity-prediction-app/app.py
@@ -46,7 +46,6 @@ st.markdown("""
 This app allows you to predict the bioactivity towards inhibting the `Acetylcholinesterase` enzyme. `Acetylcholinesterase` is a drug target for Alzheimer's disease.
 
 **Credits**
-- App built in `Python` + `Streamlit` by [Chanin Nantasenamat](https://medium.com/@chanin.nantasenamat) (aka [Data Professor](http://youtube.com/dataprofessor))
 - Descriptor calculated using [PaDEL-Descriptor](http://www.yapcwsoft.com/dd/padeldescriptor/) [[Read the Paper]](https://doi.org/10.1002/jcc.21707).
 ---
 """)
